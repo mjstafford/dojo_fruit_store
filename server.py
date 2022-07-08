1b148cad47163ba105db257a50c66041326ce225
Michael <67665934+mjstafford@users.noreply.github.com>
2022-07-08 14:30:47 -0700

    completed assignment

--- server.py
+++ server.py
@@ -8,11 +8,19 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    straw_count = request.form["strawberry"]
+    rasp_count = request.form["raspberry"]
+    apple_count = request.form["apple"]
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    student_id = request.form["student_id"]
+
+    print(f"charging {first_name} {last_name} for {int(straw_count) + int(rasp_count) + int(apple_count)} fruits")
+    return render_template("checkout.html", strawberry_count = straw_count, raspberry_count = rasp_count, apple_count = apple_count, first_name = first_name, last_name = last_name, student_id=student_id)
 
 @app.route('/fruits')         
 def fruits():
-    return render_template("fruits.html")
+    return render_template("fruits.html", fruits_list = ["apple", "blackberry", "raspberry", "strawberry"])
 
 if __name__=="__main__":   
     app.run(debug=True)    
